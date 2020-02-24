c03889d3561d335baf8efaf03b59468378ed7d49
ChloePottier <chloe.pottier@free.fr>
2020-02-24 11:18:44 +0100

    insertion projet

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Création d'un chat ligne avec réponse en temps réel après envoie de la réponse (bouton submit). ">
+    <link rel="stylesheet" href="style.css">
+    <title>Chat en ligne</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="container">
+            <div class="header-container">
+                <img src="" alt="img-chloe">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
